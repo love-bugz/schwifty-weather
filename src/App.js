960952bf39e32f7aebd761db579c3c5995e5bab6
kmcknight1 <katiemcknight1991@gmail.com>
2019-06-01 21:05:24 -0500

    weather alert bug fix

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ function Weather({ weather }) {
   return (
     <div className="weather">
       <h3>Here's the weather</h3>
-      {weather.alerts.length > 0 && alert(weather.alerts[0].title)}
+      {weather.alerts.length > 0 && <h5>Caution: {weather.alerts[0].title}</h5>}
       <h5>Temperature: {Math.round(weather.currently.temperature)}°F</h5>
       <h5>Skies: {weather.currently.summary}</h5>
       <h5>Wind: {Math.round(weather.currently.windSpeed)}mph</h5>
